f96a44d19df43af2a5904e5c376b6c7649d71317
Geo <84283433+ItzGeoYT@users.noreply.github.com>
2024-02-26 21:28:15 +0100

    Add files via upload

---
+++ theme.config.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+
+  /* Announcement */
+  banner: {
+    key: '1x.0',
+    text: (
+      <a href="#">
+        🎉 XManage v0.0.1 version.
+      </a>
+    )
+  },
+
+  /* Display the name */
+  useNextSeoProps() {
+    return {
+      titleTemplate: 'XManage - %s'
+    }
+  },
+
+  /* Embed part */
+  head: (
+    <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="XManage Docs" />
+    </>
+  ),
+
+  /* Logo */
+  logo: (
+    <> <img src="/assets/images/xmanage.png" alt="XManage" style={{ width: '50px', height: 'auto' }} /> </>
+  ),
+
+  /* GitHub button */
+  project: {
+    link: 'https://github.com/Tirito6626/xmanage',
+  },
+
+  /* Discord button */
+  chat: {
+    link: 'https://discord.gg/udp5y8uq',
+  },
+
+  /* GitHub stuff */
+  docsRepositoryBase: 'https://github.com/Tirito6626/xmanage',
+  footer: {
+    text: 'Made with ❤️',
+  },
+}
+
+export default config
